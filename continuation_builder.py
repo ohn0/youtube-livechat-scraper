82e95f792501101f0a471c9bafa0f8fb342f0968
EX1A <silverskinx@gmail.com>
2022-08-02 21:55:47 -0400

    Create continuation_builder.py

---
+++ continuation_builder.py
@@ -0,0 +1,58 @@
+import requests
+
+base_url = "https://www.youtube.com/youtubei/v1/next?"
+
+class ContinuationFetcher:
+    params = {}
+
+    def __init__(self, videoId):
+        self.initializeParams(videoId)
+
+    def initializeParams(self, videoId):
+        self.params["context"] = self.initializeContext()
+        self.params["videoId"] = videoId
+        self.params["params"] = ""
+        self.params["racyCheckOk"] = False
+        self.params["contentCheckOk"] = False
+        self.params["autonavState"] = "STATE_NONE"
+        self.params["playbackContext"] = {}
+        self.params["captionRequested"] = False
+
+    def initializeContext(self):
+        context = {}
+        context["client"] = self.initializeClient()
+        context["user"]["lockedSafetyMode"] = False
+        context["request"] = {}
+        context["clickTracking"] = {}
+        context["adSignalsInfo"] = {}
+
+        return context
+    
+    def initializeClient(self):
+        client = {}
+        client["visitorData"] = ""
+        client["remoteHost"] = ""
+        client["userAgent"] = ""
+        client["clientVersion"] = "2.20220801.00.00"
+        client["osName"] = ""
+        client["osVersion"] = ""
+        client["originalUrl"] = ""
+        client["platform"] = "DESKTOP"
+        client["clientFormFactor"] = "UNKNOWN_FORM_FACTOR"
+        client["configInfo"] = {"appInstallData" : ""}
+        client["userInterfaceTheme"] = "USER_INTERFACE_THEME_DARK"
+        client["timezone"] = ""
+        client["browserName"] = ""
+        client["browserVersion"] = ""
+        client["screenWidthPoints"] = "1920"
+        client["screenHeightPoints"] = "955"
+        client["screenPixelDensity"] = 1
+        client["screenDensityFloat"] = 1
+        client["utcOffsetMinutes"] = -240
+        client["clientScreen"] = "WATCH"
+        client["mainAppWebInfo"] = {"graftUrl": "", "webDisplayMode" : "WEB_DISPLAY_MODE_BROWSER", "isWebNativeShareAvailable" : False}
+        
+        return client
+
+
+        
