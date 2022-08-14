e54d9a7d1b33cb6ce07a22b3070519db2fb43119
EX1A <silverskinx@gmail.com>
2022-08-13 20:48:14 -0400

    primitive scraper

--- liveChatScraper.py
+++ liveChatScraper.py
@@ -7,6 +7,7 @@ from player_state import PlayerState
 from subsequent_requestor import SubsequentRequestor
 import json
 from types import SimpleNamespace
+import ast
 
 CONTINUATION_FETCH_BASE_URL = "https://www.youtube.com/youtubei/v1/next?"
 
@@ -15,6 +16,7 @@ class LiveChatScraper:
     continuation = ''
     playerState = None
     contentSet = []
+    content = ''
 
     def __init__(self, videoUrl):
         #TODO save videoID from URL
@@ -46,16 +48,18 @@ class LiveChatScraper:
         self.playerState.getNextOffset()
         self.playerState.continuation = self.continuation
         for c in content[1::]:
+            self.content += str(c["replayChatItemAction"])
             self.contentSet.append(c["replayChatItemAction"])
 
     def parseSubsequentContents(self):
         subRequestor = SubsequentRequestor(self.videoId, self.playerState)
         subRequestor.buildFetcher()
         subRequestor.makeRequest()
-        content = subRequestor.response["continuationContents"]["liveChatContinuation"]["actions"]
-        # print(content)
+        content = subRequestor.response["continuationContents"]["liveChatContinuation"]["actions"][1::]
+        # print(content.text)
         self.playerState.continuation = subRequestor.updateContinuation(subRequestor.response)
-        for c in content[1::]:
+        for c in content:
+            self.content += str(c["replayChatItemAction"])
             self.contentSet.append(c)
     
 scraper = LiveChatScraper("https://www.youtube.com/watch?v=INA6yz-x4Pk")
@@ -64,27 +68,46 @@ contents = scraper.getInitialLiveChatContents()
 scraper.parseInitialContents(contents)
 scraper.parseSubsequentContents()
 
+
+
+# for c in scraper.contentSet:
+#     print(str(c))
 for c in scraper.contentSet:
-    print(c)
+    # print(c)
+    comment = ''
+    if("replayChatItemAction" in c):
+        c = c["replayChatItemAction"]
+    if("liveChatMembershipItemRenderer" in c["actions"][0]["addChatItemAction"]["item"]):
+        comment += c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["liveChatMembershipItemRenderer"]["timestampText"]["simpleText"] 
+        comment += ": " + c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["liveChatMembershipItemRenderer"]["message"]["runs"][0]["text"]
+    elif("emoji" in c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["message"]["runs"][0]):
+        comment += c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["simpleText"]
+        comment += ": " + c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["message"]["runs"][0]["emoji"]["image"]["accessibility"]["accessibilityData"]["label"]
+    else:
+        comment += c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["simpleText"]
+        comment += ": " + c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["message"]["runs"][0]["text"]
+        # print(c["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["message"]["runs"][0]["text"])
+    print(comment)
+with open('v.json', 'w', encoding='utf-8') as writer:
+    writer.write(str(scraper.content))
 
 
 
+'''
+    step 1: 
+        Grab initial continuation value  using continuation builder and requestors, these will require the videoId
+    
+    step 2:
+        Use initial continuation value to fetch first livechat request using livechat_requestor and livechat_parser.
+        The first livechat contents will be returned in a script embedded in a HTML document, which is why the parser is required
+        to extract the contents.
 
-    '''
-        step 1: 
-            Grab initial continuation value  using continuation builder and requestors, these will require the videoId
-        
-        step 2:
-            Use initial continuation value to fetch first livechat request using livechat_requestor and livechat_parser.
-            The first livechat contents will be returned in a script embedded in a HTML document, which is why the parser is required
-            to extract the contents.
+        Ensure the continuation value updates after the livechat_requestor is done executing. The continuation value will update 
+        each time a request is made as it is used to keep track of where we are on the video's timeline.
 
-            Ensure the continuation value updates after the livechat_requestor is done executing. The continuation value will update 
-            each time a request is made as it is used to keep track of where we are on the video's timeline.
-
-        step 3:
-            Use subsequent_requestor and start a loop to grab each block of livechat data. Each time a request is made, the continuation
-            value MUST be update to ensure the next obtained block of data does not contain any duplicates or missed values.
-    '''
+    step 3:
+        Use subsequent_requestor and start a loop to grab each block of livechat data. Each time a request is made, the continuation
+        value MUST be update to ensure the next obtained block of data does not contain any duplicates or missed values.
+'''
 
 # scraper = LiveChatScraper('https://www.youtube.com/watch?v=EezhXfjR1_k')
