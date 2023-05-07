3022e0466c5baae1240c45893bdc01aa16da9106
EX1A <silverskinx@gmail.com>
2023-05-07 10:59:43 -0400

    linting membership message, pinned message, and superchat message objects

--- messages/membership_message.py
+++ messages/membership_message.py
@@ -1,45 +1,59 @@
+"""Message output for membership chat and membership join messages sent in livechat"""
 from messages.message import Message
 from messages.fragments.content import Content
 import constants.node_constants as nc
 class MembershipChatMessage(Message):
+    """Message output for membership chat and membership join messages sent in livechat"""
     membershipDurationHeader = ''
     MESSAGE_TYPES = {"chat_message": "MEMBERSHIP_CHAT", 
                      "joinMessage" : "MEMBERSHIP_JOINED"}
     MESSAGE_TYPE_MEMBER_CHAT = "Membership Chat message"
     MESSAGE_TYPE_MEMBER_JOIN = "Membership Join message"
     MESSAGE_TYPE_MEMBER_RENEWAL = "Membership Renew message"
-    MESSAGE_TYPE = None
+    message_type = None
     def __init__(self, chatAction):
         super().__init__(chatAction)
-        self.content_node = self.action[nc.ADD_CHAT_ITEM_ACTION_NODE][nc.ITEM_NODE][nc.LIVECHAT_MEMBERSHIP_NODE]
+        self.content_node = self.action[nc.ADD_CHAT_ITEM_ACTION_NODE][nc.ITEM_NODE]\
+            [nc.LIVECHAT_MEMBERSHIP_NODE]
 
-    def findMembershipMessageType(self):
-        tooltip = self.action[nc.ADD_CHAT_ITEM_ACTION_NODE][nc.ITEM_NODE][nc.LIVECHAT_MEMBERSHIP_NODE][nc.AUTHOR_BADGE_NODE][0][nc.LIVECHAT_AUTHOR_BADGE_NODE][nc.TOOLTIP_NODE]
-        if(tooltip == "New member"):
-            self.MESSAGE_TYPE = self.MESSAGE_TYPE_MEMBER_JOIN
-        elif(nc.MESSAGE_NODE in self.content_node):
-            self.MESSAGE_TYPE = self.MESSAGE_TYPE_MEMBER_CHAT
+    def __find_membership_message_type(self):
+        tooltip = self.action[nc.ADD_CHAT_ITEM_ACTION_NODE][nc.ITEM_NODE]\
+            [nc.LIVECHAT_MEMBERSHIP_NODE][nc.AUTHOR_BADGE_NODE][0]\
+                [nc.LIVECHAT_AUTHOR_BADGE_NODE][nc.TOOLTIP_NODE]
+        if tooltip == "New member":
+            self.message_type = self.MESSAGE_TYPE_MEMBER_JOIN
+        elif nc.MESSAGE_NODE in self.content_node:
+            self.message_type = self.MESSAGE_TYPE_MEMBER_CHAT
         else:
-            self.MESSAGE_TYPE = self.MESSAGE_TYPE_MEMBER_RENEWAL
+            self.message_type = self.MESSAGE_TYPE_MEMBER_RENEWAL
 
     def build_message(self):
-        self.findMembershipMessageType()
-        self.occurrence_timestamp = self.content_node[nc.TIMESTAMP_SIMPLE_TEXT_NODE][nc.SIMPLE_TEXT_NODE]
+        """builds the membership joined/chat message object for scraper
+        consumption, along with bringing over metadata."""
+        self.__find_membership_message_type()
+        self.occurrence_timestamp = self.content_node\
+            [nc.TIMESTAMP_SIMPLE_TEXT_NODE][nc.SIMPLE_TEXT_NODE]
         self.time_stamp = self.content_node[nc.TIMESTAMP_USEC_NODE]
         self.author = self.content_node[nc.AUTHOR_NODE][nc.SIMPLE_TEXT_NODE]
-        self.context_message = self.extractMembershipContextMessage()
+        self.context_message = self.__extract_membership_context_message()
 
-    def extractMembershipContextMessage(self):
-        contextOutput = ''
-        if(self.MESSAGE_TYPE == self.MESSAGE_TYPE_MEMBER_CHAT):
-            contextOutput = {"membershipChat" :  self.runs_message_builder(self.content_node[nc.MESSAGE_NODE][nc.RUNS_NODE])}
-        elif(self.MESSAGE_TYPE == self.MESSAGE_TYPE_MEMBER_JOIN):
-            contextOutput = {"membershipJoin" : self.runs_message_builder(self.content_node[nc.HEADER_SUBTEXT_NODE])}
-        elif(self.MESSAGE_TYPE == self.MESSAGE_TYPE_MEMBER_RENEWAL):
-            contextOutput = {"membershipRenewal" : self.runs_message_builder(self.content_node[nc.HEADER_SUBTEXT_NODE])}
+    def __extract_membership_context_message(self):
+        context_output = ''
+        if self.message_type == self.MESSAGE_TYPE_MEMBER_CHAT:
+            context_output = {"membershipChat" :  \
+                self.runs_message_builder(self.content_node[nc.MESSAGE_NODE][nc.RUNS_NODE])}
+        elif self.message_type == self.MESSAGE_TYPE_MEMBER_JOIN:
+            context_output = {"membershipJoin" : \
+                self.runs_message_builder(self.content_node[nc.HEADER_SUBTEXT_NODE])}
+        elif self.message_type == self.MESSAGE_TYPE_MEMBER_RENEWAL:
+            context_output = {"membershipRenewal" : \
+                self.runs_message_builder(self.content_node[nc.HEADER_SUBTEXT_NODE])}
         else:
-            contextOutput = {"membershipError" : "Unrecognized MESSAGE_TYPE"}
-        return contextOutput
+            context_output = {"membershipError" : "Unrecognized MESSAGE_TYPE"}
+        return context_output
 
     def generate_content(self):
-        return Content(self.occurrence_timestamp, self.time_stamp, self.author, self.context_message, self.MESSAGE_TYPE).object_output()
+        """returns object for output consumption"""
+        return Content(self.occurrence_timestamp, self.time_stamp, \
+            self.author, self.context_message, self.message_type).object_output()
+            
--- messages/superchat_message.py
+++ messages/superchat_message.py
@@ -1,23 +1,30 @@
-from messages.message import Message
-from messages.fragments.content import Content
+"""module ofr processing a superchat message from scraper"""
 import constants.node_constants as nc
 import constants.scraper_constants as sCons
+from messages.fragments.content import Content
+from messages.message import Message
 
 class SuperChatMessage(Message):
-    def __init__(self, action):
-        super().__init__(action)
+    """class for processing a superchat object into a message
+    for consumption"""
 
     def build_message(self):
-        self.content_node = self.action[nc.ADD_CHAT_ITEM_ACTION_NODE][nc.ITEM_NODE][nc.LIVECHAT_PAID_MESSAGE_NODE]
-        self.occurrence_timestamp = self.content_node[nc.TIMESTAMP_SIMPLE_TEXT_NODE][nc.SIMPLE_TEXT_NODE]
+        """converts a superchat message from scraper content
+        into a message object for consumption"""
+        self.content_node = self.action[nc.ADD_CHAT_ITEM_ACTION_NODE][nc.ITEM_NODE]\
+            [nc.LIVECHAT_PAID_MESSAGE_NODE]
+        self.occurrence_timestamp = self.content_node[nc.TIMESTAMP_SIMPLE_TEXT_NODE]\
+            [nc.SIMPLE_TEXT_NODE]
         self.time_stamp = self.content_node[nc.TIMESTAMP_USEC_NODE]
         self.author = self.content_node[nc.AUTHOR_NODE][nc.SIMPLE_TEXT_NODE]
         self.context_message = {
             "purchaseAmount" : self.content_node[nc.PURCHASE_AMOUNT_NODE],
-            "message" : self.runs_message_builder(self.content_node[nc.MESSAGE_NODE][nc.RUNS_NODE]) if nc.MESSAGE_NODE in self.content_node else ''
+            "message" : self.runs_message_builder(self.content_node[nc.MESSAGE_NODE]\
+                [nc.RUNS_NODE]) if nc.MESSAGE_NODE in self.content_node else ''
         }
 
     def generate_content(self):
+        """generates superchat message output for consumption"""
         return Content(
             self.occurrence_timestamp,
             self.time_stamp,
